431e4c73e0f003cbce504285754376b8c2792586
brntava <bruno_tava7@hotmail.com>
2022-05-09 15:03:53 -0300

    criado packagejson e instaldo o express

--- index.js
+++ index.js
@@ -9,7 +9,35 @@ let server = http.createServer((req, res) =>{
     console.log('URL: ', req.url);
     console.log('METHOD: ', req.method);
 
-    res.end('ok')
+    let user = [{
+        name: 'Bruno',
+        email: 'bruno@gmail.com',
+        id: 1
+    }];
+
+    // Verificar URL
+
+    switch(req.url){
+
+        case '/':
+
+            res.statusCode = 200; // Se deu tudo certo me retorna 200
+
+            res.setHeader('Content-type', 'text/html');
+            res.end('<h1>Ola</h1>');
+
+        break
+
+        case '/users':
+
+            res.statusCode = 200;
+
+            res.setHeader('Content-type', 'application/json');
+            res.end(JSON.stringify(user));
+
+        break
+
+    }
 
 });
 
